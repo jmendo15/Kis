0f4c594421c809db7b5c9edbb19e6adb0901fe39
Ray Toal <rtoal@lmu.edu>
2024-03-13 17:51:56 -0700

    Fix some parsing issues

--- test/parser.test.js
+++ test/parser.test.js
@@ -6,55 +6,81 @@
 //     });
 // });
 
-import assert from "node:assert/strict"
-import parse from "../src/parser.js"
-import { errorMonitor } from "node:events"
+import assert from "node:assert/strict";
+import parse from "../src/parser.js";
 
 //Code that Kis should compile
 // Must be of form [scenario, source_code]
 const syntaxChecks = [
-    ["variable declaration", "set variable = 3"],
-    ["print statement", "meow(hi)"],
-    ["while statement", "whisker x < 3: meow(hello) nap"],
-    ["if statement", "if x < 3: meow(hello) nap"],
-    ["if with else", "if x < 3: meow(hello) else: meow(goodbye)"],
-    ["function declaration", "kitty func(parameter: String): meow(hi) purr 0 nap"],
-    ["all logical operators", "set x = true && false || (!false)"],
-    ["ternary operator", "meow( x ? y : z)"],
-    ["comments", "meow(0) // comment"],
-    ["for loop", "fur cat in cats: meow(cap) nap"],
-    ["modularity import", "import package_example"],
-    ["modularity export", "module package_example export kitty addition(a: int, b: int): purr a + b nap"]
-]
+  ["variable declaration", "set variable = 3"],
+  ["print statement", "meow(hi)"],
+  ["while statement", "whisker x < 3: meow(hello) nap"],
+  ["if statement", "if x < 3: meow(hello) nap"],
+  ["if with else", "if x < 3: meow(hello) else: meow(goodbye) nap"],
+  [
+    "function declaration",
+    "kitty func(parameter: String): meow(hi) meow(there) purr 0 nap",
+  ],
+  ["all logical operators", "set x = true && false || (!false)"],
+  ["ternary operator", "meow( x ? y : z)"],
+  ["comments", "meow(0) // comment"],
+  ["for loop", "fur cat in cats: meow(cap) nap"],
+  ["modularity import", "import packageExample"],
+  [
+    "modularity export",
+    `module packageExample
+      export kitty addition(a: int, b: int): purr a + b nap
+    nap`,
+  ],
+];
 
 //Code that Kis should NOT compile
 // Must be of form [scenario, source_code, errorMessagePattern]
 const syntaxErrors = [
-    ["variable declaration", "set variable = 3", /Line 1, col 1/],
-    ["wrong variable declaration", "let variable = 3", /Line 1, col 1/],
-    ["wrong print statement", "print(hi)", /Line 1, col 1/],
-    ["wrong while statement", "while x < 3: meow(hello)", /Line 1, col 1/],
-    ["wrong if statement", "incase x < 3 meow(hello) sleep", /Line 1, col 1/],
-    ["wrong if with else", "if x < 3 meow(hello) orelse meow(goodbye)", /Line 1, col 6/],
-    ["function declaration without type declaration", "kitty func(parameter) meow(hi) purr 0 nap", /Line 1 col 3/],
-    ["different all logical operators", "set x = true and false or (!false)", /Line 1 col 5/],
-    ["wrong ternary operator and too many params in print statement", "meow( x question y colon z)", /Line 1 col 2/],
-    ["comments with pound symbol ", "meow(0) #comment", /Line 1 col 3/],
-    ["wrong for loop", "for cat in cats: meow(cap) nap", /Line 1 col 1/],
-    ["wrong modularity import", "importing package_example", /Line 1 col 1/],
-    ["wrong modularity export", "module package_example mail kitty addition(a: int, b: int): purr a + b", /Line 1 col 3/]
-]
+  ["wrong variable declaration", "let variable = 3", /Line 1, col 1/],
+  ["wrong print statement", "print(hi)", /Line 1, col 1/],
+  ["wrong while statement", "while x < 3: meow(hello)", /Line 1, col 1/],
+  ["wrong if statement", "incase x < 3 meow(hello) sleep", /Line 1, col 1/],
+  [
+    "missing colon after if test",
+    "if x < 3 meow(hello) orelse meow(goodbye)",
+    /Line 1, col 10/,
+  ],
+  [
+    "parameter without type declaration",
+    "kitty func(parameter) meow(hi) purr 0 nap",
+    /Line 1, col 21/,
+  ],
+  [
+    "using and instead of &&",
+    "set x = true and false or (!false)",
+    /Line 1, col 14/,
+  ],
+  [
+    "wrong ternary operator and too many params in print statement",
+    "meow( x question y colon z)",
+    /Line 1, col 9/,
+  ],
+  ["comments with pound symbol ", "meow(0) #comment", /Line 1, col 9/],
+  ["wrong for loop", "for cat in cats: meow(cap) nap", /Line 1, col 1/],
+  ["wrong modularity import", "importing packagexample", /Line 1, col 7/],
+  [
+    "wrong modularity export",
+    "module packageExample mail kitty addition(a: int, b: int): purr a + b",
+    /Line 1, col 23/,
+  ],
+];
 
 describe("The parser", () => {
-    for (const [scenario, source] of syntaxChecks) {
-        it(`properly specifies ${scenario}`, () => {
-            assert(parse(source).succeeded())
-        })
-    }
-    for (const [scenario, source, errorMessagePattern] of syntaxErrors) {
-        it(`does not permit ${scenario}`, () => {
-            let err = parse(source)
-            assert.throws(() => parse(source), err.message, err.message)
-        })
-    }
-})
+  for (const [scenario, source] of syntaxChecks) {
+    it(`properly specifies ${scenario}`, () => {
+      assert(parse(source).succeeded());
+    });
+  }
+  for (const [scenario, source, errorMessagePattern] of syntaxErrors) {
+    it(`does not permit ${scenario}`, () => {
+      //let err = parse(source)
+      assert.throws(() => parse(source), errorMessagePattern);
+    });
+  }
+});
