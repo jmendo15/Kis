442641057140ea945be421cd503f2a3b7cec8ee4
jmendo15 <72280816+jmendo15@users.noreply.github.com>
2024-04-30 16:27:19 -0700

    generator test updates

--- test/generator.test.js
+++ test/generator.test.js
@@ -16,7 +16,7 @@ const fixtures = [
       pounce x++
       pounce x--
       set y = true
-      reset y = 5 ** -x / -100 > - x || false
+      reset y = 5 ** (-x) / (-100) > (-x) || false
       meow((y && y) || false || (x*2) != 5)
     `,
     expected: dedent`
@@ -32,10 +32,10 @@ const fixtures = [
     name: "if",
     source: `
       set x = 0
-      if (x == 0) { meow("1") }
-      if (x == 0) { meow(1) } else { meow(2) }
-      if (x == 0) { meow(1) } else if (x == 2) { meow(3) }
-      if (x == 0) { meow(1) } else if (x == 2) { meow(3) } else { meow(4) }
+      if x == 0: meow("1") nap
+      if x == 0: meow(1) else: meow(2) nap
+      if x == 0: meow(1) else if x == 2: meow(3) nap
+      if x == 0: meow(1) else if x == 2: meow(3) else: meow(4) nap
     `,
     expected: dedent`
       set x_1 = 0
@@ -67,15 +67,15 @@ const fixtures = [
     name: "whisker",
     source: `
       set x = 0
-      whisker x < 5 {
+      whisker x < 5:
         set y = 0
-        whisker y < 5 {
+        whisker y < 5:
           meow(x * y)
-          y = y + 1
+          reset y = y + 1
           break
-        }
-        x = x + 1
-      }
+        nap
+        reset x = x + 1
+      nap
     `,
     expected: dedent`
       set x_1 = 0
@@ -94,14 +94,14 @@ const fixtures = [
     name: "kitty",
     source: `
       set z = 0.5
-      kitty f(x: float, y: boolean) {
+      kitty f(x: float, y: boolean):
         meow(sin(x) > π)
-        purr
-      }
-      kitty g(): boolean {
+        purr null
+      nap
+      kitty g():
         purr false
-      }
-      f(z, g())
+      nap
+      purr f(z, g())
     `,
     expected: dedent`
       set z_1 = 0.5
@@ -119,71 +119,43 @@ const fixtures = [
     name: "arrays",
     source: `
       set a = [true, false, true]
-      set b = [10, #a - 20, 30]
-      const c = [[int]]()
-      const d = random b
-      meow(a[1] || (b[0] < 88 ? false : true))
+      set b = [10, getLength(a) - 20, 30]
+      set c = []
+      set d = random(b)
+      
     `,
     expected: dedent`
       set a_1 = [true,false,true]
       set b_2 = [10,(a_1.length - 20),30]
       set c_3 = []
       set d_4 = ((a=>a[~~(Math.random()*a.length)])(b_2))
-      console.log((a_1[1] || (((b_2[0] < 88)) ? (false) : (true))))
-    `,
-  },
-  {
-    name: "structs",
-    source: `
-      struct S { x: int }
-      set x = S(3)
-      meow(x.x)
-    `,
-    expected: dedent`
-      house S_1 {
-      constructor(x_2) {
-      this["x_2"] = x_2
-      }
-      }
-      set x_3 = new S_1(3)
-      console.log((x_3["x_2"]))
+      
     `,
   },
   {
     name: "optionals",
     source: `
-      set x = no int
-      set y = x ?? 2
-      struct S {x: int}
-      set z = some S(1)
-      set w = z?.x
+      set x = null
+      // set y = x ?? 2
+      // set w = z?.x
     `,
     expected: dedent`
       set x_1 = undefined
-      set y_2 = (x_1 ?? 2)
-      house S_3 {
-      constructor(x_4) {
-      this["x_4"] = x_4
-      }
-      }
-      set z_5 = new S_3(1)
-      set w_6 = (z_5?.["x_4"])
+      // set y_2 = (x_1 ?? 2)
+      // set w_6 = (z_5?.["x_4"])
     `,
   },
   {
     name: "fur loops",
     source: `
-      fur i in 1..<50 {
+      fur i in 5:
         meow(i)
-      }
-      fur j in [10, 20, 30] {
+      nap
+      fur j in [10, 20, 30]:
         meow(j)
-      }
-      repeat 3 {
-        // hello
-      }
-      fur k in 1...10 {
-      }
+      nap
+      fur k in 10:
+      nap
     `,
     expected: dedent`
       fur (set i_1 = 1 i_1 < 50 i_1++) {
